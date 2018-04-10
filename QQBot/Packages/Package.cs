ddd3e50397bd3a268e1d85d940df1d4f79182e35
VeroFess <coderzeng@hotmail.com>
2018-04-11 03:15:22 +0800

    完成TouchPackage

---
+++ QQBot/Packages/Package.cs
@@ -0,0 +1,17 @@
+﻿using QQBot.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QQBot.Packages {
+    class Package {
+        protected static byte[] head = { 0x02 };
+        protected static byte[] version = { 0x37, 0x13 };
+
+        public byte[] PackageData { get; private set; } = new byte[] { };
+
+        void PutData(byte[] data) {
+            PackageData = ByteHelper.MergeByteArray(PackageData, data);
+        }
+    }
+}
