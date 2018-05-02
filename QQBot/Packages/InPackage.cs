38b9342e145de96eac831ef122cb4bda916b19d2
VeroFess <coderzeng@hotmail.com>
2018-05-03 04:37:31 +0800

    我说我硬盘坏了不知道你们信不信

---
+++ QQBot/Packages/InPackage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QQBot.Packages {
+    public class InPackage : Package {
+        public byte[] PackageData { get { return this._PackageData; } }
+        public int DataLength { get { return this._PackageData.Length; } }
+
+        public InPackage(byte[] Data) {
+            this._PackageData = Data;
+        }
+    }
+}
